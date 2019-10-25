b550a582eb38ce0494d416c679c6bdc414b1c609
toutatsu <56914660+toutatsu@users.noreply.github.com>
2019-10-25 16:50:06 +0900

    全点間最短距離

--- graph/Floyd-Warshall.cpp
+++ graph/Floyd-Warshall.cpp
@@ -1,6 +1,6 @@
 //Floyd-Warshall algorithm
-//Worst-case performance        O(V^3)
-//Average performance           O(V^3)
-//Best-case performanceO        O(V^3)
-//Worst-case space complexity   O(V^2)
+//Worst-case performance        O(|V|^3)
+//Average performance           O(|V|^3)
+//Best-case performanceO        O(|V|^3)
+//Worst-case space complexity   O(|V|^2)
 for(int k=0;k<v;k++)for(int i=0;i<v;i++)for(int j=0;j<v;j++)graph[i][j]=min(graph[i][j],graph[i][k]+graph[k][j]);
