696addd7bda586d24c420179213609b3c00d0c9d
toutatsu <56914660+toutatsu@users.noreply.github.com>
2019-11-16 23:14:51 +0900

    Create binary_search.cpp

---
+++ binary_search.cpp
@@ -0,0 +1,8 @@
+//めぐる式二分探索
+long long ng=-1LL,ok=1000000000000LL,mid;//ng:解が存在しない値　ok:解が存在する値
+
+    while(abs(ok-ng)>1LL){
+        mid=(ok+ng)/2LL;
+        if(solve(mid,a,f,t))ok=mid;
+        else ng=mid;
+    }
