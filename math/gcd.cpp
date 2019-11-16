5c23d122ad9fec1d7293f437e284ce0e0bec544c
toutatsu <56914660+toutatsu@users.noreply.github.com>
2019-11-17 08:42:48 +0900

    Create gcd.cpp

---
+++ math/gcd.cpp
@@ -0,0 +1,16 @@
+int gcd(int x,int y){return y?gcd(y,x%y):x;} //Greatest Common Divisor
+int lcm(int x,int y){return x*y/gcd(x,y);}   //Least Common Multiple
+//数が大きい時はx/gcd(x,y)*yにしてあふれるのを防ぐ
+
+int gcd(int x,int y){//ループ
+	int r;
+	if(x<y)swap(x,y);
+	while(y>0){r=x%y;x=y;y=r;}
+	return x;
+}
+
+int array_gcd(int a[],int size){//複数の数の最大公約数
+	int x=a[0];
+	for(int i=1;i<size;i++)x=gcd(x,a[i]);
+    return x;
+}
