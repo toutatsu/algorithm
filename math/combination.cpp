16fb6f76a81a158357f46738289206dd675e1980
toutatsu <56914660+toutatsu@users.noreply.github.com>
2019-11-16 23:01:04 +0900

    Create combination.cpp

---
+++ math/combination.cpp
@@ -0,0 +1,59 @@
+#define MOD 1000000007
+
+//--------------------------------ABC034 Cより----------------------------------------
+//powlではMODが取れず溢れるため
+//繰り返し自乗法
+
+long long power(long long a,long long x){//a^x mod MOD
+    return x?x%2?power(a,x-1)*a%MOD:(long long)powl(power(a,x/2),2)%MOD:1;
+}
+
+
+/*フェルマーの小定理
+MOD p(素数)での逆元
+a^(p-1) ≡ 1    mod(p)
+a^(p-2) ≡ a^-1 mod(p)
+*/
+long long nCr(long long n,long long r){
+    long long nCr=1LL;
+    for(long long i=n;i>n-r;nCr%=MOD,i--)nCr*=i;//分子
+    for(long long i=1;i<=r;nCr%=MOD,i++)nCr*=power(i,MOD-2);//分母
+    return nCr;
+}
+
+
+
+
+//以下パスカルの三角形による方法　大きな値は計算できない
+
+//////1//////////自作
+unsigned long long PascalsTriangle[n+1][n+1]={};//0オリジン
+
+unsigned long long combination(int n,int k){
+
+    if(PascalsTriangle[n][k])return PascalsTriangle[n][k];//すでにデータがあれば利用
+    
+    PascalsTriangle[0][0]=1ULL;//頂点
+    for(int step=1;step<=n;step++){//step段目の計算
+        for(int i=0;i<=n;i++){
+            PascalsTriangle[step][i]=(i||i==n?PascalsTriangle[step-1][i-1]+PascalsTriangle[step-1][i]:PascalsTriangle[step-1][i]);
+        }
+    //for(int i=0;i<(n+1)*(n+1);i++)cout<<PascalsTriangle[i/(n+1)][i%(n+1)]<<(i%(n+1)==n?"\n":" ");//確認
+    //cout<<endl;
+    }
+    return PascalsTriangle[n][k];
+}
+
+
+//////2//////////////
+
+------------------ABC132 他の人(chanciiさん)のcombinationより------------------------------
+int n,k,c[N][N];//<-0で初期化
+
+	for(int i=0;i<=n+1;i++)
+	{
+		c[i][0]=1;
+		for(int j=1;j<=i;j++)c[i][j]=(c[i-1][j]+c[i-1][j-1])%mod;
+	}
+
+///今回は N ≤ 2000 なので、パスカルの三角形を上から求めていく要領で
