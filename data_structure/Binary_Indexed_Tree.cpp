3dc486ea4a2cc24444e2811e66123db6d3d9538f
toutatsu <56914660+toutatsu@users.noreply.github.com>
2019-11-28 20:02:59 +0900

    Update Binary_Indexed_Tree.cpp

--- data_structure/Binary_Indexed_Tree.cpp
+++ data_structure/Binary_Indexed_Tree.cpp
@@ -1,1 +1,19 @@
 BIT
+testしろ
+int bit[n];
+
+int sum(int i){
+  int sum=0;
+  while(i>0){
+    s+=bit[i];
+    i-=i&-i;
+  }
+  return sum;
+}
+
+void add(int i,int x){
+  while(i≤n){
+    bit[i]+=x;
+    i+=i&-i;
+  }
+}
