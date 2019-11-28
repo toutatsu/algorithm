237c876f1bb4686de6b9d00a6c784eb2adebc6fb
toutatsu <56914660+toutatsu@users.noreply.github.com>
2019-11-28 13:59:43 +0900

    Update dfs.cpp

--- graph/dfs.cpp
+++ graph/dfs.cpp
@@ -1,3 +1,19 @@
-void dfs(int v){
+visited[V]=false;
 
+//recursion
+void dfs(int v){
+  visited[v]=true;
+  for(all adjacence vertex)if(!visited[vertex])dfs(x);
 }
+
+
+
+//stack
+
+
+
+stack<int>s;
+
+
+s.push_back(0);
+visited[0]=true;
