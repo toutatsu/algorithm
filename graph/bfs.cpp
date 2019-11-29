d7dbb33a15f6ad29d535c6b241ed63d72c44e66f
toutatsu <56914660+toutatsu@users.noreply.github.com>
2019-11-29 22:06:25 +0900

    Update bfs.cpp

--- graph/bfs.cpp
+++ graph/bfs.cpp
@@ -1,9 +1,10 @@
-//テストしろ
+//ABC146D verified
+
     queue<int>q;
     q.push(0);
+    visited[0]=true;
+
     while(!q.empty()){
         int p=q.front();q.pop();
-        for(auto x:graph[p]){
-            if(!visited[x]){visited[x]=true;q.push(x);}
-        }
+        for(auto& x:graph[p])if(!visited[x]){visited[x]=true;q.push(x);}
     }
