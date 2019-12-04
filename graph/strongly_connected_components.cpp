8de8be7ab6a125f8e0810feaeb98e3a7b0daba80
toutatsu <56914660+toutatsu@users.noreply.github.com>
2019-12-04 15:17:38 +0900

    講義のも後で足す

--- graph/strongly_connected_components.cpp
+++ graph/strongly_connected_components.cpp
@@ -1,12 +1,32 @@
-
+//蟻本p279
 //O(|V|+|E|)
-strongly_connected_components
 
-vector<T>graph[V],rgraph[V]
-dfs(){
+vector<T>graph[V],rgraph[V],vs;//vsは帰りがけの順
+visited[vertex];
+//recursion
 
+void dfs(int v){
+    visited[v]=true;
+    for(all adjacence vertex)if(!visited[vertex])dfs(x);
+    vs.push_back(v);
 }
 
-rdfs(){
-
+void rdfs(int v,int k){
+    visited[v]=true;
+    comp[v]=k;
+    //rgraph
+    for(all adjacence vertex)if(!visited[vertex])dfs(x,k);
 }
+
+int scc(){
+    for(all vertex)if(!visited[i])dfs(i);
+    for(all)visited[]=false;
+    for(int i=vs.size()-1,k=0;i>=0;i--){
+        if(visited[vs[i]])rdfs(vs[i],k++);
+    }
+    return k;
+}
+
+
+
+
