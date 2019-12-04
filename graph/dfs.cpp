055a69ee652da2226fdc7ff232228bb5c72cbb13
toutatsu <56914660+toutatsu@users.noreply.github.com>
2019-12-04 15:04:31 +0900

    stack version ?

--- graph/dfs.cpp
+++ graph/dfs.cpp
@@ -2,18 +2,20 @@ visited[V]=false;
 
 //recursion
 void dfs(int v){
-  visited[v]=true;
-  for(all adjacence vertex)if(!visited[vertex])dfs(x);
+    visited[v]=true;
+    for(all adjacence vertex)if(!visited[vertex])dfs(x);
 }
 
 
 
 //stack
 
-
-
 stack<int>s;
-
 
 s.push_back(0);
 visited[0]=true;
+
+while(!s.empty()){
+    int p=s.top();s.pop();
+    for(all adjacence vertex)if(!visited[vertex])s.push(vertex),visited[vertex]=true;
+}
