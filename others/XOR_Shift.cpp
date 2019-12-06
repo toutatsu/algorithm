a1d37c0954eb7687869878924201739aba68c2e5
toutatsu <56914660+toutatsu@users.noreply.github.com>
2019-12-06 10:26:07 +0900

    Create XOR_Shift.cpp

---
+++ others/XOR_Shift.cpp
@@ -0,0 +1,27 @@
+
+
+
+
+
+class XOR_Shift{
+
+	unsigned int x, y, z, w, t;
+public:
+	XorShift() {
+		x = 133553533;
+		y = 314867339;
+		z = 664298413;
+		w = 999999937;
+		t = 0;
+	}
+	unsigned int rand() {
+		t = x ^ (x << 11);
+		x = y;
+		y = z;
+		z = w;
+		w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+		return w & 0x7fffffff;
+	}
+
+
+}
