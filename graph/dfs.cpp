633fe4bdac16371e14a30e4f73d1a08d1c662dcc
toutatsu <56914660+toutatsu@users.noreply.github.com>
2019-12-26 01:10:41 +0900

    add dfs

--- graph/dfs.cpp
+++ graph/dfs.cpp
@@ -19,3 +19,18 @@ while(!s.empty()){
     int p=s.top();s.pop();
     for(all adjacence vertex)if(!visited[vertex])s.push(vertex),visited[vertex]=true;
 }
+
+
+//木に対しては単一最短経路のコストをdfsで求められる ABC070 D
+
+long long depth[v];
+
+void dfs(int current_v,int parent,long long cost){
+    depth[current_v]=cost;
+    for(auto x:g[current_v])if(x.to!=parent)dfs(x.to,current_v,cost+x.cost);//(木の子孫は全て未訪問)
+    return;
+}
+
+int main(){
+    dfs(root,-1,0LL);
+}
