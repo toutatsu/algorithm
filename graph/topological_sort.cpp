5e53d776466c0fb9b33d1ec7c116a60a68a1b992
toutatsu <56914660+toutatsu@users.noreply.github.com>
2019-12-26 00:25:40 +0900

    Update

--- graph/topological_sort.cpp
+++ graph/topological_sort.cpp
@@ -13,7 +13,29 @@ vector<int> t_sort(vector<int>g[]){
             if(indeg[x]==0)Q.push(x);//入次数が0になったら追加
         }
     }
+    //もし入次数が0にでない頂点が残っていたらDAGでない
     return ret;
 }
 
-//深さ優先探索版も追加する
+
+//wikipedia 3つ目のコード
+//AOJ GRL_4_B Topological Sort verified
+
+vector<int>t_sort; //← トポロジカルソートされた結果の入る空の連結リスト
+int visited[v]; //0:未訪問 1:一時的に訪問 2:恒久的に訪問
+
+bool dfs(int v){
+    if(visited[v]==1)return false;//閉路があり DAG でないので中断
+    else if(visited[v]==0){//未訪問
+        visited[v]=1;//n に「一時的」の印を付ける
+        for(auto x:g[v])dfs(x);
+        visited[v]=2;
+        t_sort.push_back(v);//vを先頭に追加
+    }
+}
+
+int main(){
+
+    for(int i=0;i<v;i++)if(!visited[i])dfs(i);
+    reverse(t_sort.begin(),t_sort.end());//逆順に
+}
