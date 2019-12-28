8f809282ed80820aee9714bebb865065eff45e57
toutatsu <56914660+toutatsu@users.noreply.github.com>
2019-12-28 18:14:56 +0900

    verified

--- graph/strongly_connected_components.cpp
+++ graph/strongly_connected_components.cpp
@@ -1,32 +1,36 @@
-//蟻本p279
+//蟻本p286
 //O(|V|+|E|)
-
-vector<T>graph[V],rgraph[V],vs;//vsは帰りがけの順
-visited[vertex];
-//recursion
+//GRL_3_C: Strongly Connected Components verified
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int>g[V],rg[V],vs;//vsは帰りがけの順
+bool visited[V];
+int cmp[V];
 
 void dfs(int v){
     visited[v]=true;
-    for(all adjacence vertex)if(!visited[vertex])dfs(x);
+    for(auto x:g[v])if(!visited[x])dfs(x);
     vs.push_back(v);
 }
-
-void rdfs(int v,int k){
+void rdfs(int v,int cnt){
     visited[v]=true;
-    comp[v]=k;
-    //rgraph
-    for(all adjacence vertex)if(!visited[vertex])dfs(x,k);
+    cmp[v]=cnt;
+    for(auto x:rg[v])if(!visited[x])rdfs(x,cnt);
 }
 
-int scc(){
-    for(all vertex)if(!visited[i])dfs(i);
-    for(all)visited[]=false;
-    for(int i=vs.size()-1,k=0;i>=0;i--){
-        if(visited[vs[i]])rdfs(vs[i],k++);
+int main(){
+    int v,e,s,t,q,cnt=0;
+    cin>>v>>e;
+    for(int i=0;i<e;i++){
+        cin>>s>>t;
+        g[s].push_back(t);
+        rg[t].push_back(s);
     }
-    return k;
+    
+    for(int i=0;i<v;i++)if(!visited[i])dfs(i);
+    for(auto &x:visited)x=false;
+    for(int i=v-1;i>=0;i--)if(!visited[vs[i]])rdfs(vs[i],cnt++);
+    
+    return 0;
 }
-
-
-
-
