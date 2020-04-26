0267f9ac0822b2d2eb52456aa2b22579bc5816d2
toutatsu <56914660+toutatsu@users.noreply.github.com>
2020-04-27 01:46:21 +0900

    Update Graph.cpp

--- graph/Graph.cpp
+++ graph/Graph.cpp
@@ -6,14 +6,14 @@ using namespace std;
 
 template<typename T>
 class Graph{
-    //struct edge{int to;T cost;};
-    struct edge{int to;T cap,rev;}; //ford_fulkerson
+    struct edge{int to;T cost;};
+    //struct edge{int to;T cap,rev;}; //ford_fulkerson
 
-    
     public:
     int Vertex_num,Edge_num;
     vector<edge>*g;
-    vector<edge>*edges;//for Bellman-Ford
+    vector<bool>visited;//dfs用
+    //vector<edge>edges;//for Bellman-Ford
 
     const T INF=numeric_limits< T >::max();//#include<limits>
 
@@ -21,35 +21,37 @@ class Graph{
         Vertex_num=V;
         Edge_num=E;
         g=new vector<edge>[V];
-        edges=new vector<edge>(E);//for Bellman-Ford
+        visited=vector<bool>(Vertex_num,false);
+        // //edges=new vector<edge>(E);//for Bellman-Ford
     }
 
     void append(int from,int to,T cost){
         edge e={to,cost};
         g[from].push_back(e);
-        edges->push_back(e);//for Bellman-Ford
+        //edges->push_back(e);//for Bellman-Ford
     }
 
     void input(bool direct,bool weight){
         int from,to;
         T cost;
         for(int i=0;i<Edge_num;i++){
-            if(weight)cin>>from>>to>>cost;
-            else {cin>>from>>to;cost=1;}
+            cin>>from>>to;
+            if(weight)cin>>cost;
+            else cost=1;
             this->append(from,to,cost);
             if(!direct)this->append(to,from,cost);
         }
     }
 
-    void flow_input(){
-        int from,to;
-        T cap;
-        for(int i=0;i<Edge_num;i++){
-            cin>>from>>to>>cap;
-            g[from].push_back(edge{to,cap,int(g[to].size())});
-            g[to].push_back(edge{from,0,int(g[from].size()-1)});
-        }
-    }
+//     // void flow_input(){
+//     //     int from,to;
+//     //     T cap;
+//     //     for(int i=0;i<Edge_num;i++){
+//     //         cin>>from>>to>>cap;
+//     //         g[from].push_back(edge{to,cap,int(g[to].size())});
+//     //         g[to].push_back(edge{from,0,int(g[from].size()-1)});
+//     //     }
+//     // }
 
     void check(){
         for(int i=0;i<Vertex_num;i++){
@@ -58,13 +60,14 @@ class Graph{
         }
     }
 
-    vector<bool>visited=new vector<bool>(Vertex_num);//dfs用
-    vector<int>dfs(int start){
+    
+    void dfs(int start){
         visited[start]=true;
+        cout<<start<<"visit"<<endl;
         for(auto x:g[start])if(!visited[x.to])dfs(x.to);
     }
 
-    vector<T>bfs(int start){
+    vector<T>bfs(int start){//重み無しの時最短経路になる？
         vector<T>dist(Vertex_num,-1);//訪問:startからの距離 未訪問:-1
         queue<int>q;
         q.push(start);
@@ -100,25 +103,25 @@ class Graph{
         return dist;
     }
 
-    vector<T>bellman_ford(int start){
-        //Bellman-Ford algorithm
-        //Worst-case performance        O(|V||E|)
-        //Average performance           
-        //Best-case performance         O(|E|)
-        //Worst-case space complexity   O(|V|)
-        //蟻本89p参考
-        vector<T>dist(Vertex_num,INF);
-        dist[start]=0;
-        for(int i=0;i<Vertex_num;i++){
-            for(int j=0;j<Edge_num;j++){
-                if(dist[edges[j].to]>dist[edges[j].from]+edges[j].cost){
-                    dist[edges[j].to]=dist[edges[j].from]+edges[j].cost;
-                    if(i==Vertex_num-1)return vector<int>(1,-1);//負閉路
-                }
-            }
-        }
-        return dist;
-    }
+//     // vector<T>bellman_ford(int start){
+//     //     //Bellman-Ford algorithm
+//     //     //Worst-case performance        O(|V||E|)
+//     //     //Average performance           
+//     //     //Best-case performance         O(|E|)
+//     //     //Worst-case space complexity   O(|V|)
+//     //     //蟻本89p参考
+//     //     vector<T>dist(Vertex_num,INF);
+//     //     dist[start]=0;
+//     //     for(int i=0;i<Vertex_num;i++){
+//     //         for(int j=0;j<Edge_num;j++){
+//     //             if(dist[edges[j].to]>dist[edges[j].from]+edges[j].cost){
+//     //                 dist[edges[j].to]=dist[edges[j].from]+edges[j].cost;
+//     //                 if(i==Vertex_num-1)return vector<int>(1,-1);//負閉路
+//     //             }
+//     //         }
+//     //     }
+//     //     return dist;
+//     // }
 
     vector<int>topological_sort(){
         //O(V+E)
@@ -140,53 +143,61 @@ class Graph{
     }
 
 
-//     T flow_dfs(int v,int sink,T f){//増加パスを探す
-//         if(v==sink)return f;
-//         visited[0][v]=true;
-//         for(int i=0;i<g[v].size();i++){
-//             edge &e=g[v][i];
-//             if(!visited[0][e.to]&&e.cap>0){
-//                 T d=flow_dfs(e.to,sink,min(f,e.cap));
-//                 if(d>0){
-//                     e.cap-=d;
-//                     g[e.to][e.rev].cap+=d;
-//                     return d;
-//                 }
-//             }
-//         }
-//         return 0;
-// }
-//     T ford_fulkerson(int source,int sink){
-//         //sourceからsinkへの最大流量flowを求める
-//         T flow,f;
-//         for(flow=0;(f=flow_dfs(source,sink,INF))!=0;flow+=f)for(auto& x:visited[0])x=false;
-//         return flow;
-//     }
-//     // typedef struct{int to,cap,rev;}flow_edge;
-//     // vector<edge>g[v];//グラフの隣接リスト
-//     // bool visited[v];//dfsで使用
+// //     T flow_dfs(int v,int sink,T f){//増加パスを探す
+// //         if(v==sink)return f;
+// //         visited[0][v]=true;
+// //         for(int i=0;i<g[v].size();i++){
+// //             edge &e=g[v][i];
+// //             if(!visited[0][e.to]&&e.cap>0){
+// //                 T d=flow_dfs(e.to,sink,min(f,e.cap));
+// //                 if(d>0){
+// //                     e.cap-=d;
+// //                     g[e.to][e.rev].cap+=d;
+// //                     return d;
+// //                 }
+// //             }
+// //         }
+// //         return 0;
+// // }
+// //     T ford_fulkerson(int source,int sink){
+// //         //sourceからsinkへの最大流量flowを求める
+// //         T flow,f;
+// //         for(flow=0;(f=flow_dfs(source,sink,INF))!=0;flow+=f)for(auto& x:visited[0])x=false;
+// //         return flow;
+// //     }
+// //     // typedef struct{int to,cap,rev;}flow_edge;
+// //     // vector<edge>g[v];//グラフの隣接リスト
+// //     // bool visited[v];//dfsで使用
 
 
-    //----------------------Tree------------------------//
+};
+template<typename T>
+class Tree : public Graph<T>{
+    //Graph<T>:: this->と同じ？
+    public:
+    vector<T>depth,parent;//dfs用
+
+    Tree(int V):Graph<T>::Graph(V,V-1){
+        depth=vector<T>(Graph<T>::Vertex_num,0);
+        parent=vector<T>(Graph<T>::Vertex_num,-1);
+    };
 
     //木に対しては単一最短経路のコストをdfsで求められる ABC070 D, ABC 126 D,ABC 148 F verified 
-    vector<T>*depth=new vector<T>(Vertex_num,0),*parent=new vector<T>(Vertex_num,-1);
-    vector<T> Tree_dfs(int current_v,int parent_v,T cost){
+
+    void Tree_dfs(int current_v,int parent_v,T cost){
         parent[current_v]=parent_v;
         depth[current_v]=cost;
-        for(auto x:g[current_v])if(x.to!=parent_v)dfs(x.to,current_v,cost+x.cost);//(木の子孫は全て未訪問)
+        for(auto x:Graph<T>::g[current_v])if(x.to!=parent_v)dfs(x.to,current_v,cost+x.cost);//(木の子孫は全て未訪問)
     }
-    void tree_dfs(int root){
-        Tree_dfs(root,-1,0LL);//再帰関数の呼び出し
-    }
+    void dfs(int root){Tree_dfs(root,-1,0LL);}//再帰関数の呼び出し
 
     T tree_diameter(){
-        vector<T>a=this->bfs(0);
+        vector<T>a=Graph<T>::bfs(0);
         int idx=-1,ma=-1;
-        for(int i=0;i<Vertex_num;i++)if(ma<a[i])ma=a[i],idx=i;
-        a=this->bfs(idx);
+        for(int i=0;i<Graph<T>::Vertex_num;i++)if(ma<a[i])ma=a[i],idx=i;
+        a=Graph<T>::bfs(idx);
         ma=-1;
-        for(int i=0;i<Vertex_num;i++)if(ma<a[i])ma=a[i];
+        for(int i=0;i<Graph<T>::Vertex_num;i++)if(ma<a[i])ma=a[i];
         return ma;
     }
 
@@ -205,7 +216,7 @@ class Graph{
 
         //parent初期化O(NlogN)
         for(int k=0;k+1<20;k++){
-            for(int v=0;v<Vertex_num;v++){
+            for(int v=0;v<Graph<T>::Vertex_num;v++){
                 if(parent[k][v]<0)parent[k+1][v]=-1;
                 else parent[k+1][v]=parent[k][parent[k][v]];
             }
@@ -235,8 +246,9 @@ class Graph{
 int V,E;
 int main(){
     cin>>V>>E;
-    Graph<long long>graph(V,E);
-    graph.flow_input();
-    //graph.ford_fulkerson(0,V-1);
+    Tree<long long>t(V);
+    t.input(false,false);
+    t.check();
+    cout<<t.tree_diameter()<<endl;
     return 0;
 }
