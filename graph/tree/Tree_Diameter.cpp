638206802abd779e8e8a4bfb8112263aaa40d85e
toutatsu <56914660+toutatsu@users.noreply.github.com>
2020-04-28 12:51:04 +0900

    Update Tree_Diameter.cpp

--- graph/tree/Tree_Diameter.cpp
+++ graph/tree/Tree_Diameter.cpp
@@ -1,12 +1,22 @@
 
-
+//AOJ GRL_5_A: Diameter of a Tree Verified
+//bfsが必要
 template<typename T>
 T tree_diameter(Tree<T>tree){
-    vector<T>a=Graph<T>::bfs(0);
+    vector<T>a=bfs(tree,0);
     int idx=-1,ma=-1;
-    for(int i=0;i<Graph<T>::Vertex_num;i++)if(ma<a[i])ma=a[i],idx=i;
-    a=Graph<T>::bfs(idx);
+    for(int i=0;i<tree.Vertex_num;i++)if(ma<a[i])ma=a[i],idx=i;
+    a=bfs(tree,idx);
     ma=-1;
-    for(int i=0;i<Graph<T>::Vertex_num;i++)if(ma<a[i])ma=a[i];
+    for(int i=0;i<tree.Vertex_num;i++)if(ma<a[i])ma=a[i];
     return ma;
+}
+
+int N;
+int main(){
+    cin>>N;
+    Tree<long long>tree(N);
+    tree.input(false,true);
+    cout<<tree_diameter(tree)<<endl;
+    return 0;
 }
